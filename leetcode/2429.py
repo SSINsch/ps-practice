0a70ea812bef5c8503b6490773768af2d5a305a6
SSINsch <cksgh1996@gmail.com>
2025-01-15 22:11:47 +0900

    leetcode 2429 another solution

--- leetcode/2429.py
+++ leetcode/2429.py
@@ -40,6 +40,38 @@ class Solution:
         return ans_decimal
 
 
+class SolutionBitwise:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        # num2의 1의 개수
+        num_set2 = bin(num2).count('1')
+
+        # num1의 1의 개수
+        num_set1 = bin(num1).count('1')
+
+        # 초기 결과값: num1을 기반으로 생성
+        result = num1
+
+        if num_set1 > num_set2:
+            # 더 많은 비트를 제거: 가장 낮은 비트부터 1을 제거
+            for i in range(32):  # 최대 32비트
+                if result & (1 << i):  # i번째 비트가 1인지 확인
+                    result ^= (1 << i)  # i번째 비트를 0으로 변경
+                    num_set1 -= 1
+                if num_set1 == num_set2:
+                    break
+
+        elif num_set1 < num_set2:
+            # 부족한 비트를 추가: 가장 낮은 비트부터 1로 설정
+            for i in range(32):
+                if not (result & (1 << i)):  # i번째 비트가 0인지 확인
+                    result |= (1 << i)  # i번째 비트를 1로 변경
+                    num_set1 += 1
+                if num_set1 == num_set2:
+                    break
+
+        return result
+
+
 s = Solution()
 print(s.minimizeXor(3, 5))
 print(s.minimizeXor(1, 12))
